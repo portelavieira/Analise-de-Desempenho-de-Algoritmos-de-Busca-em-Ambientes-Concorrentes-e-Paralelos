15b5352b87c25c78abe3a7938ab16f50d8b92997
Vinishows <104833002+Vinishows@users.noreply.github.com>
2024-10-29 18:20:23 -0300

    Insertion Sort

---
+++ AV2/src/InsertionSort/InsertionSort.java
@@ -0,0 +1,17 @@
+package InsertionSort;
+// InsertionSort.java
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // Move os elementos que são maiores que a chave para uma posição à frente
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
